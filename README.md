7e4c47ff58e72513b5114602ffcf530c1e53557b
phil <phil@branch14.org>
2016-11-25 14:13:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# codegraph
+
+Generates a dot file based on your Clojure/ClojureScript code.
+
+## Installation
+
+`git clone https://gitlab.com/200ok/codegraph.git`
+
+## Usage
+
+    $ lein run <path-to-clj-file>
+
+## Options
+
+None.
+
+## Examples
+
+Spoiler: Running codegraph on itself will give you this...
+
+![codegraph graph of codegraph itself](codegraph.png)
+
+### Bugs
+
+`(or :bugfree :plenty)`
+
+## License
+
+Copyright © 2016 phil@200ok.ch
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/codegraph/core.clj
@@ -0,0 +1,66 @@
+(ns codegraph.core
+  (:require [clojure.walk :as walk]
+            [clojure.pprint :as pprint])
+  (:gen-class))
+
+(def triggered (atom false))
+
+(def current (atom nil))
+
+(def relations (atom {}))
+
+(def trigger #{"def"
+               "defn"
+               "defn-"
+               "defmulti"
+               "defmethod"})
+
+(defn trigger? [sym]
+  (contains? trigger sym))
+
+(defn register-current! [sym]
+  (reset! triggered false)
+  (reset! current sym))
+
+(defn register-relation! [sym]
+  (swap! relations update-in [@current] conj sym))
+
+(defmulti process type)
+
+(defmethod process :default [form] form)
+
+(defmethod process clojure.lang.Symbol [form]
+  (let [sym (name form)]
+    (if (trigger? sym)
+      (reset! triggered true)
+      (if @triggered
+        (register-current! sym)
+        (register-relation! sym)))))
+
+(defn draw-one [a b]
+  (if (contains? (set (keys @relations)) b)
+    (str "\"" a "\" -> \"" b "\"")))
+
+(defn draw-many [a b]
+  (map #(draw-one a %) b))
+
+(defn graph [path]
+  (read-string (str "[" (slurp path) "]")))
+
+(defn template [lines]
+  (flatten
+   ["# call with ` | dot -Tsvg > codegraph.svg`"
+    "digraph G {"
+    "edge ["
+    "arrowtail = \"none\""
+    "arrowhead = \"open\""
+    "]"
+    lines
+    "}"]))
+
+(defn output [lines]
+  (doall (map println (template (remove nil? lines)))))
+
+(defn -main [& args]
+  (walk/prewalk process (graph (first args)))
+  (output (mapcat (partial apply draw-many) @relations)))
